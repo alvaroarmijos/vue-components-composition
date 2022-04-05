aee4661c9efe660ec46b2bc40a74a701fa8ec4e9
Alvaro Armijos <alvaro.armijoss@gmail.com>
2022-04-04 23:02:14 -0500

    ciclos de vida

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  
+  <div>{{text}}</div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components:{}
+  data() {
+    return {
+      text: "Hola Vue"
+    }
+  },
+  beforeCreate() {
+    console.log("beforeCreate", this.$data, this.$el);
+  },
+  created() {
+    console.log("created", this.$data, this.$el);
+    },
+  mounted() {
+    console.log("mounted", this.$data, this.$el);
+    },
 };
 </script>
 

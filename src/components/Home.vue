3d470dd9c02d9ea7679d083e78776cbe445bc0d2
Alvaro Armijos <alvaro.armijoss@gmail.com>
2022-04-06 08:41:53 -0500

    watchers

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>{{obj}}</div>
+  <div>{{obj.counter}}</div>
 </template>
 
 <script>
-import { reactive} from "vue";
+import { reactive, watch} from "vue";
 
 export default {
   setup() {
     const obj = reactive({counter:0});
 
     setInterval(() => obj.counter++, 500);
+
+    watch(()=>obj.counter,(valor, anterior)=>{
+      console.log(valor, anterior);
+    });
     
     return {
       obj
